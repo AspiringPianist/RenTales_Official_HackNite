ae566513662ea0ae622cc70d3152116fe0d803c9
Prakrititz Borah <143780392+SweetBunny123@users.noreply.github.com>
2024-04-07 01:38:00 +0530

    path_referencing

--- python_scripts/story_gen.py
+++ python_scripts/story_gen.py
@@ -6,17 +6,31 @@ from reorganize import ScriptWriter
 from request import ImageGen
 from base_generator import StoryGen
 from pillow_bg import clear_bg
-from ..config.options import renpath, path_to_intermediate_scripts
+import sys
+import os
 
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Now you can import the module using relative import
+from config.options import renpath, path_to_intermediate_scripts
 #prompt = input()
 # Generating the base story and then building a human readable script
-prompt = 'a script on the adventures of harry potter and his friends'
+prompt = ''
+with open('./python_scripts/prompt.txt', 'r') as f:
+    prompt = f.read()
+print('prompt is: ', prompt)
 storygen = StoryGen(prompt)
+print('Generating Base Story..')
 storygen.generate_base()
+print('Generated Base Story!')
+print('Now generating script..')
 storygen.generate_story()
+print('Generated script!!')
 
 # Parsing story to RenPy compatible format and analyzing mood for music simulatneously
 parser = Parser()
+print('Parsing the script')
 parser.read_script(f'{path_to_intermediate_scripts}/output.txt', f'{path_to_intermediate_scripts}/script2.rpy')
 print(f'Written raw script to {path_to_intermediate_scripts}/script2.rpy')
 # Reordering and syntax correction
